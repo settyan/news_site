2f2b8b88d3518be1af320040f7705a0240d17b78
settyan <senaredline@gmail.com>
2020-08-29 10:55:10 +0900

    :bug: cors

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -32,7 +32,10 @@ export default new Vuex.Store({
     async fetchHeadline({ state, commit }) {
       commit("setIsLoadingHeadline", true);
       const response = await fetch(
-        `${process.env.VUE_APP_PROXY_URL}/${process.env.VUE_APP_API_URL}/v2/top-headlines?country=jp&apiKey=${process.env.VUE_APP_API_KEY}&pageSize=${state.maxHeadlineSize}`
+        `${process.env.VUE_APP_PROXY_URL}/${process.env.VUE_APP_API_URL}/v2/top-headlines?country=jp&apiKey=${process.env.VUE_APP_API_KEY}&pageSize=${state.maxHeadlineSize}`,
+        {
+          mode: "cros"
+        }
       );
       const json = await response.json();
       const headline = json.articles || [];
